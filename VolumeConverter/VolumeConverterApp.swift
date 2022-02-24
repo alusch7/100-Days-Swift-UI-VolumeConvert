2f661829b0a8d3e37a0a919dbca4cc66ef9fd87a
alusch7 <luschera@mcmaster.ca>
2022-02-23 20:52:40 -0500

    Initial Commit

---
+++ VolumeConverter/VolumeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeConverterApp.swift
+//  VolumeConverter
+//
+//  Created by Alden Luscher on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct VolumeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
